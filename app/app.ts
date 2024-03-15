c0ab76c6ade337e41e001aac455368807584b738
kwonssshyeon <kwonsh4396@gmail.com>
2024-03-16 04:20:37 +0900

    make dockerfile

--- app/app.ts
+++ app/app.ts
@@ -10,7 +10,7 @@ export const createApp = (client: RedisClient) => {
   app.use(express.json());
 
   app.get("/", (request, response) => {
-    response.status(200).send("hello from express, deploy on AWS Lightsail with solo workflow");
+    response.status(200).send("hello from express, deploy on AWS Lightsail with solo workflow !");
   });
 
   function fibonacci(n:number):number{
