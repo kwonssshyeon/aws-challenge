3f5c91d07e30b0eb70a935e8413f7ad51328e451
kwonssshyeon <kwonsh4396@gmail.com>
2024-03-16 16:44:46 +0900

    excute graceful shutdown

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM node:18
+# Build stage
+
+FROM node:18 as build
+
+WORKDIR /usr/src/my-app
 
 COPY package*.json .
 
@@ -8,4 +12,17 @@ COPY . .
 
 RUN npm run build
 
-CMD ["npm","start"]
+# Product stage
+
+FROM node:18 as production
+
+WORKDIR /usr/src/my-app
+
+# Build stage의 build파일을 복사해서 build경로에 넣는다.
+COPY --from=Build ./usr/src/my-app/build ./build
+COPY --from=build ./usr/src/my-app/package.json ./package.json
+COPY --from=build ./usr/src/my-app/package-lock.json ./package-lock.json
+
+RUN npm install --only=production
+
+CMD ["node","build/index.js"]
--- app/index.ts
+++ app/index.ts
@@ -14,9 +14,24 @@ const startServer = async () => {
   await client.connect();
 
   const app = createApp(client);
-  app.listen(PORT, () => {
+  const server = app.listen(PORT, () => {
     console.log(`App listening at port ${PORT}`);
+  });
+
+  return server;
+};
+
+const server = startServer();
+
+const gracefulShutdown = async () => {
+  const _server = await server;
+  _server.close(()=> {
+    console.log("graceful shutdown!");
+    process.exit();
   });
 };
 
-startServer();
+
+process.on("SIGTERM", gracefulShutdown)
+
+process.on("SIGINT", gracefulShutdown)
